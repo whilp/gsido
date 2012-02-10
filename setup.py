8cfd650540f4dc04c79f55404859d1047fa11e6a
Will Maier <wcmaier@hep.wisc.edu>
2012-02-10 08:59:52 -0600

    Add build infra.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+meta = dict(
+    name          = "gsido",
+    version       = "0.1",
+    description   = "Run commands using glexec",
+    author        = "Will Maier",
+    author_email  = "wcmaier@hep.wisc.edu",
+    url           = "http://hg.hep.wisc.edu/cmsops/gsido",
+    scripts       = ["gsido"],
+)
+
+setup(**meta)
